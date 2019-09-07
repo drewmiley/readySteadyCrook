2a950c86e7e072246ae0f32ba33b410d3d6b7f87
Drew Miley <drewmiley92@gmail.com>
2019-09-07 13:38:26 +0100

    Helpful constants

--- draw.js
+++ draw.js
@@ -1,25 +1,27 @@
-const getFillRect = (ctx, imageCtx, text, offset, textWidthPerLetter, rectRand, rowSpacing) => (i, j, k) => {
+const getFillRect = (ctx, imageCtx, text, offset, { textWidths, totalTextWidth }, rectRand, rowSpacing) => (i, j, k) => {
     const totalOffset = i * offset;
     const startHeight = i * rowSpacing;
+    const startWidth = j * totalTextWidth + textWidths.slice(0, k).reduce((acc, d) => acc + d, 0);
     const rectColor = imageCtx.getImageData(
-        (j * text.length + k + (rectRand ? Math.random() : 0.5)) * textWidthPerLetter - totalOffset,
+        startWidth - totalOffset + (rectRand ? Math.random() : 0.5) * textWidths[k],
         startHeight + (rectRand ? Math.random() : 0.5) * rowSpacing,
         1, 1
     ).data;
     ctx.fillStyle = `rgba(${rectColor[0]}, ${rectColor[1]}, ${rectColor[2]}, ${rectColor[3] / 255})`;
     ctx.fillRect(
-        (j * text.length + k) * textWidthPerLetter - totalOffset,
+        startWidth - totalOffset,
         startHeight,
-        (j * text.length + k + 1) * textWidthPerLetter - totalOffset,
+        startWidth - totalOffset + textWidths[k],
         startHeight + rowSpacing
     );
 }
 
-const getFillText = (ctx, imageCtx, text, offset, spacing, textWidthPerLetter, letterRand, rowSpacing) => (i, j, k) => {
+const getFillText = (ctx, imageCtx, text, offset, spacing, { textWidths, totalTextWidth }, letterRand, rowSpacing) => (i, j, k) => {
     const totalOffset = i * offset;
     const startHeight = i * rowSpacing;
+    const startWidth = j * totalTextWidth + textWidths.slice(0, k).reduce((acc, d) => acc + d, 0);
     const color = imageCtx.getImageData(
-        (j * text.length + k + (letterRand ? Math.random() : 0.5)) * textWidthPerLetter - totalOffset,
+        startWidth - totalOffset + (letterRand ? Math.random() : 0.5) * textWidths[k],
         startHeight + (letterRand ? Math.random() : 0.5) * rowSpacing,
         1, 1
     ).data;
@@ -27,7 +29,7 @@ const getFillText = (ctx, imageCtx, text, offset, spacing, textWidthPerLetter, l
     ctx.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${255 / 255})`;
     ctx.fillText(
         text[k],
-        (j * text.length + k) * textWidthPerLetter - totalOffset,
+        startWidth - totalOffset,
         startHeight - spacing
     );
 }
@@ -53,10 +55,18 @@ function draw(canvas, imageCanvas, img, { text, size, offset, spacing, font, bac
 
     ctx.font = `bold ${size}px ${font}`;
 
-    const textWidthPerLetter = ctx.measureText(text).width / text.length;
+    const textWidths = text.split('').map(d => ctx.measureText(d).width);
+
+    const cachedTextWidths = textWidths.map((d, i) => {
+        return {
+            value: d,
+            sum: textWidths.reduce((acc, d) => acc + d, 0)
+        }
+    })
+    const totalTextWidth = textWidths.reduce((acc, d) => acc + d, 0);
 
     // Calculate columns required
-    const columns = Math.ceil(textWidthPerLetter * canvas.width);
+    const columns = Math.ceil(totalTextWidth * canvas.width / text.length);
     const columnsRequiredWithOffset = Math.ceil((columns * size) / (size - offset));
 
     // Calculate rows required
@@ -65,8 +75,8 @@ function draw(canvas, imageCanvas, img, { text, size, offset, spacing, font, bac
 
     console.log(`Drawing Rows Total ${rows}`);
 
-    const fillRect = getFillRect(ctx, imageCtx, text, offset, textWidthPerLetter, rectRand, rowSpacing);
-    const fillText = getFillText(ctx, imageCtx, text, offset, spacing, textWidthPerLetter, letterRand, rowSpacing);
+    const fillRect = getFillRect(ctx, imageCtx, text, offset, { textWidths, totalTextWidth }, rectRand, rowSpacing);
+    const fillText = getFillText(ctx, imageCtx, text, offset, spacing, { textWidths, totalTextWidth }, letterRand, rowSpacing);
 
     for (let i = 0; i < rows; i++) {
         console.log(`Percentage Complete: ${100 * i / rows}%`);
