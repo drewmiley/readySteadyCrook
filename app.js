68d2cbb2e3535f10c78d61be35d5db9066010dd8
Drew Miley <drew.miley@komododigital.co.uk>
2019-09-19 16:56:10 +0100

    File input works

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-function app() {
-    var imageCanvas = document.createElement('canvas');
-    var canvas = document.createElement('canvas');
+var imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+var runButton = document.getElementById('run');
+var canvas = document.getElementById('canvas');
+var imageCanvas = document.createElement('canvas');
 
-    var img = new Image();
+function handleImage(e) {
+  var reader = new FileReader();
+  reader.onload = e => {
+      var img = new Image();
+      img.onload = function() {
+          imageCanvas.height = img.naturalHeight;
+          imageCanvas.width = img.naturalWidth;
+          var imageCtx = imageCanvas.getContext('2d');
+          imageCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+          function app() {
+            return run(img);
+          }
+          runButton.onclick = app;
+      }
+      img.crossOrigin = "Anonymous";
+      img.src = event.target.result;
+  }
+  reader.readAsDataURL(e.target.files[0]);     
+}
 
-    img.onload = function() {
-        const options = {
-            text: document.getElementById('text').value.split(','),
-            size: parseInt(document.getElementById('size').value),
-            offset: parseInt(document.getElementById('offset').value),
-            offsetRows: parseInt(document.getElementById('offsetRows').value),
-            spacing: parseInt(document.getElementById('spacing').value),
-            font: document.getElementById('font').value,
-            backgroundColor: document.getElementById('backgroundColor').value,
-            backgroundImage: document.getElementById('backgroundImageTrue').checked,
-            preview: document.getElementById('previewTrue').checked
-        }
-        console.log('Running');
-        draw(canvas, imageCanvas, img, options);
-    };
-
-    img.crossOrigin = "Anonymous";
-    img.src = `./${document.getElementById('imageName').value}.jpg`;
-    document.body.appendChild(canvas);
+const run = image => {
+    const options = {
+        text: document.getElementById('text').value.split(','),
+        size: parseInt(document.getElementById('size').value),
+        offset: parseInt(document.getElementById('offset').value),
+        offsetRows: parseInt(document.getElementById('offsetRows').value),
+        spacing: parseInt(document.getElementById('spacing').value),
+        font: document.getElementById('font').value,
+        backgroundColor: document.getElementById('backgroundColor').value,
+        backgroundImage: document.getElementById('backgroundImageTrue').checked,
+        preview: document.getElementById('previewTrue').checked
+    }
+    console.log('Running');
+    draw(canvas, imageCanvas, image, options);
 }
