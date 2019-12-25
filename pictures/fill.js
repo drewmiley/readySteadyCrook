976fe9a35babbd091a97651bfd6dec405df3fd3d
Drew Miley <drewmiley92@gmail.com>
2019-12-25 12:40:17 +0000

    Declares concentrate options

--- pictures/fill.js
+++ pictures/fill.js
@@ -50,6 +50,9 @@ const getDistortionPixelInit = (ctx, smallCanvas, distortionOptions) => i => j =
 }
 
 const getConcentrationFill = (getLargeCanvasData, startWidth, startHeight, x, y, concentrateOptions) => {
+    const concentrateWidth = concentrateOptions.x;
+    const concentrateHeight = concentrateOptions.y;
+    const concentrateDecay = concentrateOptions.decay;
     const largeColor = getLargeCanvasData(startWidth, startHeight, x, y);
     const r = Math.round(largeColor[0]);
     const g = Math.round(largeColor[1]);
