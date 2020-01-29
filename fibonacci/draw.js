310407874c1f5b3a7a7524bc717837ad6bbc0705
Drew Miley <drewmiley92@gmail.com>
2020-01-29 20:38:42 +0000

    Initial square in correct position

--- fibonacci/draw.js
+++ fibonacci/draw.js
@@ -18,27 +18,26 @@ function draw(canvas, imageCanvas, img) {
     const hDiff = (img.naturalHeight - width) / 2;
     const wDiff = (img.naturalWidth - width) / 2;
 
-    console.log(hDiff, wDiff, img.naturalHeight - hDiff, img.naturalWidth - wDiff, imageCanvas.height, imageCanvas.width);
+    console.log(wDiff, hDiff, img.naturalWidth - wDiff, img.naturalHeight - hDiff, 0, 0, imageCanvas.width, imageCanvas.height);
     console.log(img.naturalHeight - 2 * hDiff, img.naturalWidth - 2 * wDiff);
 
-    // TODO: Figure out why not square
-    ctx.drawImage(img, wDiff, hDiff, img.naturalWidth - wDiff, img.naturalHeight - hDiff, 0, 0, imageCanvas.width, imageCanvas.height);
+    imageCtx.drawImage(img, wDiff, hDiff, width, width, 0, 0, width, width);
 
     console.log(`Drawing Rows Total ${fibonacciNumbers.length}`);
     const start = Date.now();
-    //
-    // // for (let i = 0; i < fibonacciNumbers.length; i++) {
-    // for (let i = 0; i < 1; i++) {
-    //     if (i > 0) {
-    //       const timeLeft = (fibonacciNumbers.length - i) * (Date.now() - start) / i;
-    //       console.log(`Seconds Left: ${Math.floor(timeLeft / 1000)}`);
-    //     }
-    //     // TODO: Figure out x and y
-    //     // TODO: Also https://www.w3schools.com/tags/canvas_rotate.asp
-    //     const x = 0;
-    //     const y = 0;
-    //     ctx.drawImage(imageCanvas, 0, 0, height, height, x, y, fibonacciNumbers[i], fibonacciNumbers[i]);
-    // }
+
+    // for (let i = 0; i < fibonacciNumbers.length; i++) {
+    for (let i = 0; i < 1; i++) {
+        if (i > 0) {
+          const timeLeft = (fibonacciNumbers.length - i) * (Date.now() - start) / i;
+          console.log(`Seconds Left: ${Math.floor(timeLeft / 1000)}`);
+        }
+        // TODO: Figure out x and y
+        // TODO: Also https://www.w3schools.com/tags/canvas_rotate.asp
+        const x = 0;
+        const y = 0;
+        ctx.drawImage(imageCanvas, 0, 0, width, width, x, y, fibonacciNumbers[i], fibonacciNumbers[i]);
+    }
 
     console.log('Done');
 }
