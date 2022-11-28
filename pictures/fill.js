92db33a07de2edf889fe38e9147a4b0305a8910d
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-11-28 19:35:22 +0000

    Fixes concentrate

--- pictures/fill.js
+++ pictures/fill.js
@@ -141,23 +141,23 @@ const getConcentrationPixel = ({ widthValues, heightValues }, largeCanvasData, s
     const widthValue = widthValues[startWidth + x];
     const heightValue = heightValues[startHeight + y];
     if (widthValue >= 0 && heightValue >= 0) {
-          const nwColor = largeCanvasData[Math.floor(widthValue)][Math.floor(heightValue)];
-          const neColor = largeCanvasData[Math.ceil(widthValue)][Math.floor(heightValue)];
-          const swColor = largeCanvasData[Math.floor(widthValue)][Math.ceil(heightValue)];
-          const seColor = largeCanvasData[Math.ceil(widthValue)][Math.ceil(heightValue)];
-          const widthRemainder = widthValue % (Math.floor(widthValue) || 1);
-          const heightRemainder = heightValue % (Math.floor(heightValue) || 1);
-          const propNW = Array.from(nwColor).map(d => d * (widthRemainder + heightRemainder) / 3);
-          const propNE = Array.from(neColor).map(d => d * (widthRemainder + 1 - heightRemainder) / 3);
-          const propSW = Array.from(swColor).map(d => d * (heightRemainder + 1 - widthRemainder) / 3);
-          const propSE = Array.from(seColor).map(d => d * (1 - widthRemainder - heightRemainder) / 3);
-          const r = propNW[0] + propNE[0] + propSW[0] + propSE[0];
-          const g = propNW[1] + propNE[1] + propSW[1] + propSE[1];
-          const b = propNW[2] + propNE[2] + propSW[2] + propSE[2];
-          const a = propNW[3] + propNE[3] + propSW[3] + propSE[3];
-          return `rgba(${Math.round(r)}, ${Math.round(g)}, ${Math.round(b)}, ${Math.round(a) / 255})`;
-          return { r: Math.round(r), g: Math.round(g), b: Math.round(b), a: Math.round(a) }
-          return [Math.round(r), Math.round(g), Math.round(b), Math.round(a) / 255];
+        const nwColor = largeCanvasData[Math.floor(widthValue)][Math.floor(heightValue)];
+        const neColor = largeCanvasData[Math.ceil(widthValue)][Math.floor(heightValue)];
+        const swColor = largeCanvasData[Math.floor(widthValue)][Math.ceil(heightValue)];
+        const seColor = largeCanvasData[Math.ceil(widthValue)][Math.ceil(heightValue)];
+        const widthRemainder = widthValue % (Math.floor(widthValue) || 1);
+        const heightRemainder = heightValue % (Math.floor(heightValue) || 1);
+        const propNW = Array.from(nwColor).map(d => d * (widthRemainder + heightRemainder) / 3);
+        const propNE = Array.from(neColor).map(d => d * (widthRemainder + 1 - heightRemainder) / 3);
+        const propSW = Array.from(swColor).map(d => d * (heightRemainder + 1 - widthRemainder) / 3);
+        const propSE = Array.from(seColor).map(d => d * (1 - widthRemainder - heightRemainder) / 3);
+        const r = propNW[0] + propNE[0] + propSW[0] + propSE[0];
+        const g = propNW[1] + propNE[1] + propSW[1] + propSE[1];
+        const b = propNW[2] + propNE[2] + propSW[2] + propSE[2];
+        const a = propNW[3] + propNE[3] + propSW[3] + propSE[3];
+        return [Math.round(r), Math.round(g), Math.round(b), Math.round(a)];
+    } else {
+        return [0, 0, 0, 0];
     }
 }
 
