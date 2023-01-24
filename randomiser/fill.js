8d3f35626f09d0bd5b0230e481285bf4123ddd04
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-01-24 23:02:47 +0000

    Initial attempt at working code

--- randomiser/fill.js
+++ randomiser/fill.js
@@ -40,13 +40,14 @@
 // }
 
 const fillRandomiserText = (ctx, imageCtx, height, width) => text => {
-    const color = [0, 0, 0];
+    const color = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
     ctx.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${255 / 255})`;
-    const startWidth = 0;
-    const startHeight = 0;
-    ctx.fillText(
-        text,
-        startWidth,
-        startHeight
-    );
+    const startWidth = Math.random() * height;
+    const startHeight = Math.random() * width;
+    const angle = Math.random() * 2 * Math.PI;
+    ctx.translate(startWidth, startHeight);
+    ctx.rotate(angle);
+    ctx.fillText(text, 0, 0);
+    ctx.rotate(-1 * angle);
+    ctx.translate(-1 * startWidth, -1 * startHeight);
 };
