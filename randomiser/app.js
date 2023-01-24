e076ecc09ed6d5cf5baa87bc2cbe5a60752743cb
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-01-24 16:11:57 +0000

    Duplicates words code to make initial randomiser folder

---
+++ randomiser/app.js
@@ -0,0 +1,34 @@
+const imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+    let reader = new FileReader();
+    reader.onload = e => {
+        let img = new Image();
+        img.onload = () => document.getElementById('run').onclick = () => run(img);
+        img.crossOrigin = "Anonymous";
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+}
+
+const run = image => {
+    const canvas = document.getElementById('canvas');
+    const imageCanvas = document.createElement('canvas');
+    const options = {
+        text: document.getElementById('text').value.split(','),
+        size: parseInt(document.getElementById('size').value),
+        wordStart: parseInt(document.getElementById('wordStart').value),
+        wordEnd: parseInt(document.getElementById('wordEnd').value),
+        offset: parseInt(document.getElementById('offset').value),
+        offsetRows: parseInt(document.getElementById('offsetRows').value),
+        spacing: parseInt(document.getElementById('spacing').value),
+        font: document.getElementById('font').value,
+        backgroundColor: document.getElementById('backgroundColor').value,
+        textColor: document.getElementById('textColor').value,
+        backgroundImage: document.getElementById('backgroundImageTrue').checked,
+        preview: document.getElementById('previewTrue').checked
+    }
+    console.log('Running');
+    draw(canvas, imageCanvas, image, options);
+}
