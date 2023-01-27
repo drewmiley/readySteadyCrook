43ff26ce9fbf71b34817fdb50f1b0c0abf27b680
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-01-27 21:28:55 +0000

    Adds <br /> on randomiser radio and adds framework to implement random functions

--- randomiser/randomiserTypes.js
+++ randomiser/randomiserTypes.js
@@ -1,11 +1,41 @@
-const powerOneRandom = () => {
-  return Math.random();
-}
+const powerOneRandom = () => Math.random();
+
+const powerTwoRandom = () => Math.random();
+
+const minusPowerTwoRandom = () => Math.random();
+
+const powerThreeRandom = () => Math.random();
+
+const minusPowerThreeRandom = () => Math.random();
+
+const sinRandom = () => Math.random();
+
+const cosRandom = () => Math.random();
+
+const sinSquaredRandom = () => Math.random();
+
+const cosSquaredRandom = () => Math.random();
 
 const getRandom = randomiserType => () => {
     switch(randomiserType) {
         case 'PowerOne':
             return powerOneRandom();
+        case 'PowerTwo':
+            return powerTwoRandom();
+        case 'MinusPowerTwo':
+            return minusPowerTwoRandom();
+        case 'PowerThree':
+            return powerThreeRandom();
+        case 'MinusPowerThree':
+            return minusPowerThreeRandom();
+        case 'Sin':
+            return sinRandom();
+        case 'Cos':
+            return cosRandom();
+        case 'SinSquared':
+            return sinSquaredRandom();
+        case 'CosSquared':
+            return cosSquaredRandom();
         default:
             return Math.random();
     }
