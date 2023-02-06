319102f573f594e0840fdcc049512bdd8a267ac9
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-02-06 23:28:22 +0000

    Implements power based randomiser

--- randomiser/randomiserTypes.js
+++ randomiser/randomiserTypes.js
@@ -1,91 +1,39 @@
-// const powerOneRandom = () => Math.random();
-//
-// const powerTwoRandom = () => Math.pow(Math.random(), 2);
-//
-// const minusPowerTwoRandom = () => 1 - Math.pow(Math.random(), 2);
-//
-// const powerThreeRandom = () => Math.pow(Math.random(), 3);
-//
-// const minusPowerThreeRandom = () => 1 - Math.pow(Math.random(), 3);
-//
-// const powerTwoMinusHalfRandom = () => Math.random() > 0.5 ? powerTwoRandom() : minusPowerTwoRandom();
-//
-// const minusPowerTwoMinusHalfRandom = () => Math.random() > 0.5 ? 0.5 * minusPowerTwoRandom() : 1 - 0.5 * minusPowerTwoRandom();
-//
-// const powerThreeMinusHalfRandom = () => Math.random() > 0.5 ? powerThreeRandom() : minusPowerThreeRandom();
-//
-// const minusPowerThreeMinusHalfRandom = () => Math.random() > 0.5 ? 0.5 * minusPowerThreeRandom() : 1 - 0.5 * minusPowerThreeRandom();
-//
-// const sinRandom = () => Math.sin(Math.PI * Math.random() / 2);
-//
-// const cosRandom = () => Math.cos(Math.PI * Math.random() / 2);
-//
-// const sinSquaredRandom = () => Math.pow(Math.sin(Math.PI * Math.random() / 2), 2);
-//
-// const cosSquaredRandom = () => Math.pow(Math.cos(Math.PI * Math.random() / 2), 2);
+const getRandomToPower = power => Math.pow(Math.random(), power);
+
+const getDefault = power => getRandomToPower(power);
+
+const getOneMinus = power => 1 - getRandomToPower(power);
+
+const getSymmetric = power => Math.random() > 0.5 ? 0.5 * getOneMinus(power) : 1 - 0.5 * getOneMinus(power);
+
+const getSymmetricInverse = power => Math.random() > 0.5 ? getDefault(power) : getOneMinus(power);
+
+const getSin = power => Math.pow(Math.sin(Math.PI * Math.random() / 2), power);
+
+const getCos = power => Math.pow(Math.cos(Math.PI * Math.random() / 2), power);
 
 const getRandom = ({ type, value, power, steps }) => () => {
     if (value) return value;
     let random = null;
-    // TODO: Implement powers
     switch(type) {
         case 'Default':
-            random = 1;
+            random = getDefault(power);
             break;
         case 'OneMinus':
-            random = 1;
+            random = getOneMinus(power);
             break;
         case 'Symmetric':
-            random = 1;
+            random = getSymmetric(power);
             break;
         case 'SymmetricInverse':
-            random = 1;
+            random = getSymmetricInverse(power);
             break;
-        case 'SinNew':
-            random = 1;
+        case 'Sin':
+            random = getSin(power);
             break;
-        case 'CosNew':
-            random = 1;
+        case 'Cos':
+            random = getCos(power);
             break;
-        // case 'PowerOne':
-        //     random = powerOneRandom();
-        //     break;
-        // case 'PowerTwo':
-        //     random = powerTwoRandom();
-        //     break;
-        // case 'MinusPowerTwo':
-        //     random = minusPowerTwoRandom();
-        //     break;
-        // case 'PowerThree':
-        //     random = powerThreeRandom();
-        //     break;
-        // case 'MinusPowerThree':
-        //     random = minusPowerThreeRandom();
-        //     break;
-        // case 'PowerTwoMinusHalf':
-        //     random = powerTwoMinusHalfRandom();
-        //     break;
-        // case 'MinusPowerTwoMinusHalf':
-        //     random = minusPowerTwoMinusHalfRandom();
-        //     break;
-        // case 'PowerThreeMinusHalf':
-        //     random = powerThreeMinusHalfRandom();
-        //     break;
-        // case 'MinusPowerThreeMinusHalf':
-        //     random = minusPowerThreeMinusHalfRandom();
-        //     break;
-        // case 'Sin':
-        //     random = sinRandom();
-        //     break;
-        // case 'Cos':
-        //     random = cosRandom();
-        //     break;
-        // case 'SinSquared':
-        //     random = sinSquaredRandom();
-        //     break;
-        // case 'CosSquared':
-        //     random = cosSquaredRandom();
-        //     break;
         default:
             random = Math.random();
             break;
